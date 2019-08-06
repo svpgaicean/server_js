42301b9f0182810aa7b825e5d4cf47507680ab36
Patrik Gaicean <patrik.gaicean@softvision.ro>
2019-08-06 16:49:51 +0300

    Refactor project with Express framework

---
+++ routes/categories.js
@@ -0,0 +1,24 @@
+const categories = require('../categories.json');
+const products = require('../products.json')
+
+exports.list = (req, res) => {
+	res = categories;
+	console.log(res);
+}
+
+exports.id = (req, res) => {
+	let id = Number(req.params.id);
+	let data = categories.filter(obj => obj.id === id);
+	let ids = data[0].products;
+	let response = [];
+	
+	for (let i = 0; i < products.length; i++) {
+		for (let j = 0; j < ids.length; j++) {
+			if (products[i].categories[j] === ids[j]) {
+				response.push(products[i]);
+			}
+		}
+	}
+
+	console.log(response);
+}
--- server.js
+++ server.js
@@ -1,4 +1,26 @@
-let http = require('http');
-let router = require('./routes');
+/**
+ * Module dependencies.
+ */
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const category = require('./routes/categories');
+const product = require('./routes/products');
 
-http.createServer(router.handleRequest).listen(8000);
+module.exports = app;
+
+// Config
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Categories
+app.get('/categories/', category.list);
+app.get('/categories/:id/products', category.id);
+
+// Products
+app.get('/products/', product.list);
+app.get('/products/:id', product.id);
+
+if (!module.parent) {
+	app.listen(5000);
+	console.log('Express started on port 5000');
+}
